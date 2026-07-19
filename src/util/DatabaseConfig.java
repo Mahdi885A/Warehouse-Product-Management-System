981a8d46c34a5a338bd8f99f985eaa62eb06b978
Mahdi <mahdiazimi.885@gmail.com>
2026-07-19 16:25:00 +0330

    feat: add DatabaseConfig

---
+++ src/util/DatabaseConfig.java
@@ -0,0 +1,26 @@
+package util;
+
+import exception.DatabaseConfigException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConfig {
+    public static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    public static final String USERNAME = "postgres";
+    public static final String PASSWORD = "ZqX.7391";
+
+    public Connection getConnection (){
+        try (Connection connection = DriverManager.getConnection(URL,USERNAME,PASSWORD)){
+            System.out.println("Connected...");
+            return connection;
+
+        }
+        catch (SQLException e){
+            throw new DatabaseConfigException("Database can not connected! "+e.getMessage() );
+        }
+    }
+
+}
+
